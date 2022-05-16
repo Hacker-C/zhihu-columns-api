9e007c8cf3c88c4600fce1163c37d0e6dc7af6ca
Hacker-C <1507559148@qq.com>
2022-05-16 20:07:29 +0800

    ✨ 添加统一返回数据出口函数

---
+++ middlewares/ResultHandler.js
@@ -0,0 +1,13 @@
+// TIP 返回数据统一出口
+module.exports = (results, req, res, next) => {
+  if (results.code === 1) {
+    console.log(results)
+    results.data = null
+    res.status(500).send(results)
+  } else if (results.code === 2) {
+    results.data = null
+    res.status(404).send(results)
+  } else {
+    res.status(200).send(results)
+  }
+}
