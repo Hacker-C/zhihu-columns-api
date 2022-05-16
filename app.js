d1c9975a917f187bce42a67c812165e89b32042f
Hacker-C <1507559148@qq.com>
2022-05-16 19:41:49 +0800

    🩹 添加错误处理中间件

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ const app = express()
 const BASE_CONFIG = require('./config/base.config')
 const routes = require('./controllers')
 const cors = require('cors')
+const errorHandler = require('./middlewares/ErrorHandler')
 const CorsConfig = require('./config/cors.config')
 
 app.use(express.json()) // 解析 req body 表单数据
 app.use(cors(CorsConfig))
 app.use(logger('dev')) // 监听接口测试情况
 app.use(BASE_CONFIG.API, routes.columnsRoutes)
+app.use(errorHandler)
 
 app.listen(BASE_CONFIG.PORT, () => {
   console.log(`serving at http://localhost:${BASE_CONFIG.PORT}`)
