c649a0e5f6bd558850d609760a0894e2991463b2
Hacker-C <1507559148@qq.com>
2022-05-17 11:52:44 +0800

    💡 添加注释

--- common/ResponseData.js
+++ common/ResponseData.js
@@ -1,5 +1,6 @@
 module.exports = class ResponseData {
   constructor(code, data, msg) {
+    // TIP code 为 0 表示 200，1 表示 500， 2 表示 404
     this.code = code
     this.msg = msg
     this.data = data
