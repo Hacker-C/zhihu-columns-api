0a15c0d2729b31b8d04a28cae2c04ab3cb7b4189
Hacker-C <1507559148@qq.com>
2022-05-17 12:10:30 +0800

    🗑️ 删除无用代码

--- middlewares/ResultHandler.js
+++ middlewares/ResultHandler.js
@@ -1,7 +1,6 @@
 // TIP 返回数据统一出口
 module.exports = (results, req, res, next) => {
   if (results.code === 1) {
-    console.log(results)
     results.data = null
     res.status(500).send(results)
   } else if (results.code === 2) {
@@ -10,4 +9,5 @@ module.exports = (results, req, res, next) => {
   } else {
     res.status(200).send(results)
   }
+  next()
 }
